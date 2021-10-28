864a79ddaaa4ec0b5cd5a97f0efa7fdf9f5fb8c5
mwangemi <mwangemik@gmail.com>
2021-10-28 15:02:52 +0300

    add products and categories

--- src/data.js
+++ src/data.js
@@ -21,3 +21,56 @@ export const sliderItems = [
       bg: "f5fafd"
    }
 ]
+
+export const categories = [
+   {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1603252109303-2751441dd157?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
+      title: "SHIRT STYLE!",
+   },
+   {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1604335079267-a79baf28e30d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1384&q=80",
+      title: "LOUNGEWEAR LOVE",
+   },
+   {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1611593006970-1b8cd8a27873?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
+      title: "LIGHT JACKETS",
+   }
+]
+
+export const popularProducts = [
+   {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+   },
+   {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1548207800-8c16d068bf17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+   },
+   {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1575425939273-46ecee6d6931?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=869&q=80"
+   },
+   {
+      id: 4,
+      img: "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+   },
+   {
+      id: 5,
+      img: "https://images.unsplash.com/photo-1588850561407-ed78c282e89b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=436&q=80"
+   },
+   {
+      id: 6,
+      img: "https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80"
+   },
+   {
+      id: 7,
+      img: "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=580&q=80"
+   },
+   {
+      id: 8,
+      img: "https://images.unsplash.com/photo-1617391258031-f8d80b22fb35?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=464&q=80"
+   }
+]
